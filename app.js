824c7c823672e7c7027f99d4ef19657d4b022bf1
abraham <abrahamkifle1212@gmail.com>
2026-01-17 13:42:55 +0300

    adding writeFile system

--- app.js
+++ app.js
@@ -1,8 +1,17 @@
 const fs = require("fs")
 
-fs.readFile("./example.txt", "utf-8", (err, data) => {
+// fs.readFile("./example.txt", "utf-8", (err, data) => {
+//     if (err) {
+//         console.log(err)
+//     }
+//     console.log(data)
+// })
+
+const content = 'Hello, NodeJs'
+
+fs.writeFile("output.txt", content, (err) => {
     if (err) {
-        console.log(err)
+        console.error(err)
     }
-    console.log(data)
+    console.log("File written successfully")
 })
