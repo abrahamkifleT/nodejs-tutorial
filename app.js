4d3c5ab09f1e178df91383f3c1a8d61b89f73db2
abraham <abrahamkifle1212@gmail.com>
2026-01-18 13:05:13 +0300

    refactor: comment out previous examples and add OS module demo
    - Commented out fs read/write operations
    - Commented out http server creation
    - Commented out path module examples
    - Added OS platform and architecture logging

--- app.js
+++ app.js
@@ -1,4 +1,7 @@
 const fs = require("fs")
+const http = require("http")
+const path = require("path")
+const os = require("os")
 
 // fs.readFile("./example.txt", "utf-8", (err, data) => {
 //     if (err) {
@@ -7,11 +10,35 @@ const fs = require("fs")
 //     console.log(data)
 // })
 
-const content = 'Hello, NodeJs'
+// const content = 'Hello, NodeJs'
 
-fs.writeFile("output.txt", content, (err) => {
-    if (err) {
-        console.error(err)
-    }
-    console.log("File written successfully")
-})
+// fs.writeFile("output.txt", content, (err) => {
+//     if (err) {
+//         console.error(err)
+//     }
+//     console.log("File written successfully")
+// })
+
+
+// const server = http.createServer((req, res) => {
+//     res.statusCode = 200
+//     res.setHeader("Content-Type", "text/plain")
+//     res.end("Hello World")
+// })
+
+
+// server.listen(3000, () => {
+//     console.log("Server running at http://localhost:3000/")
+// })
+
+
+// const directory = '/user/local'
+// const fileName = 'example.txt'
+
+// const fullPath = path.join(directory, fileName)
+// console.log(fullPath)
+
+console.log("platform", os.platform())
+console.log("CPU Architecture", os.arch())
+
+
