c6cebf6fe4541e0f71c3cfe5e1d3f1db056119ae
abraham <abrahamkifle1212@gmail.com>
2026-01-18 13:08:06 +0300

    feat: Log total and free system memory.

--- app.js
+++ app.js
@@ -40,5 +40,6 @@ const os = require("os")
 
 console.log("platform", os.platform())
 console.log("CPU Architecture", os.arch())
-
+console.log("Total Memory", os.totalmem())
+console.log("Free Memory", os.freemem())
 
