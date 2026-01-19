766cc4cd04666fae16a92a388114c01cdabb777c
abraham <abrahamkifle1212@gmail.com>
2026-01-19 21:30:08 +0300

    feat: Implement POST /submit endpoint to process request body data and update 404 message.

--- server.js
+++ server.js
@@ -1,13 +1,20 @@
 const http = require("http")
 
 const server = http.createServer((req, res) => {
-    if (req.method === "GET" && req.url === "/") {
-        res.writeHead(200, { "content-type": "text/plain" })
-        res.end("Welcome to HomePage")
+    if (req.method === "POST" && req.url === "/submit") {
+        let body = ""
+        req.on("data", (chunk) => {
+            body += chunk.toString()
+        })
+
+        req.on("end", () => {
+            res.writeHead(200, {"content-type": "application/json"})
+            res.end(JSON.stringify({message: "Data received", data: body}))
+        })
     }else{
         res.writeHead(404, {"content-type": "text/plain"})
-        res.end("Page Not Found")
-    }
+        res.end("Route not found")
+    } 
 })
 
 const PORT = 3000
