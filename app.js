63b9d2f01cf4d273ff10f4d85dc2251acaf5dc07
abraham <abrahamkifle1212@gmail.com>
2026-01-19 21:16:23 +0300

    feat: add basic routing with 404 handling to server.js and enable HTTP server in app.js

--- app.js
+++ app.js
@@ -25,16 +25,16 @@ const lodash = require("lodash")
 // })
 
 
-// const server = http.createServer((req, res) => {
-//     res.statusCode = 200
-//     res.setHeader("Content-Type", "text/plain")
-//     res.end("Hello World")
-// })
+const server = http.createServer((req, res) => {
+    res.statusCode = 200
+    res.setHeader("Content-Type", "text/plain")
+    res.end("Hello World")
+})
 
 
-// server.listen(3000, () => {
-//     console.log("Server running at http://localhost:3000/")
-// })
+server.listen(3000, () => {
+    console.log("Server running at http://localhost:3000/")
+})
 
 
 // const directory = '/user/local'
