2f379547e1ffe3a12336e51c9bac9b2ed03ba199
abraham <abrahamkifle1212@gmail.com>
2026-01-20 13:47:19 +0300

    feat: Integrate `logRequest` middleware into the server's request handling.

--- server.js
+++ server.js
@@ -2,22 +2,24 @@ const http = require("http")
 const url = require("url")
 
 // Middleware Function for logging requests
-function logRequest(req, res, next){
- console.log(`${req.method} request made to ${req.url}`) 
- next()
+function logRequest(req, res, next) {
+    console.log(`${req.method} request made to ${req.url}`)
+    next()
 }
 
 const server = http.createServer((req, res) => {
-const {pathname} = url.parse(req.url)
+    logRequest(req, res, (req, res) => {
+        const { pathname } = url.parse(req.url)
 
-if(pathname.startsWith("/user/")){
-    const userId = pathname.split("/")[2]
-    res.writeHead(200, {"content-type": "text/plain"})
-    res.end(`User ID: ${userId}`)
-}else{
-    res.writeHead(404, {"content-type": "text/plain"})
-    res.end("Route Not found")
-}
+        if (pathname.startsWith("/user/")) {
+            const userId = pathname.split("/")[2]
+            res.writeHead(200, { "content-type": "text/plain" })
+            res.end(`User ID: ${userId}`)
+        } else {
+            res.writeHead(404, { "content-type": "text/plain" })
+            res.end("Route Not found")
+        }
+    })
 })
 
 const PORT = 3000
